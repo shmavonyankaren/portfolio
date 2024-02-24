79090b1cfa27c221b5faf4dbf245d877ec202fd7
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-02-24 21:49:02 +0400

    create AppWrap.js

---
+++ src/wrapper/AppWrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavigationDots, SocialMedia } from "../componetens";
+const AppWrap = (Component, idName, classNames) =>
+  function HOC() {
+    return (
+      <div className={`app__container ${classNames}`} id={idName}>
+        <div className="app__wrapper app__flex ">
+          <Component />
+
+          <div className="copyright">
+            <p className="app_wrapper_text">@2023 Karen</p>
+            <p className="app_wrapper_text">All rights reserved</p>
+          </div>
+        </div>
+        <NavigationDots active={idName} />
+      </div>
+    );
+  };
+
+export default AppWrap;
