16d563e274d4a2b74fa65fb03c4dc6866e4cccf7
shmavonyankaren <karen.shmavonyan.2020@mail.ru>
2024-02-24 21:34:41 +0400

    creat App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./componetens";
+import "./App.scss";
+import StarsBackground from "./componetens/StarsBackground/StarsBackground";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="star-container">
+        <StarsBackground />
+      </div>
+      <div className="app">
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
